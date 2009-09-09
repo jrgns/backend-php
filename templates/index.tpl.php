372ab8866f6d4dd29e4989c3b0cbf36cc03a1372
Jurgens du Toit <jrgns@jrgns.net>
2009-09-09 11:36:10 +0200

    Stub for tracking code

--- templates/index.tpl.php
+++ templates/index.tpl.php
@@ -66,16 +66,6 @@
 			&nbsp;
 		</div>
 		{tpl:scripts.tpl.php}
-	<?php if (!$LocalSite): ?>
-		<script type="text/javascript">
-			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-		</script>
-		<script type="text/javascript">
-			var pageTracker = _gat._getTracker("UA-3257244-1");
-			pageTracker._initData();
-			pageTracker._trackPageview();
-		</script>
-	<?php endif; ?>
+		{tpl:tracking.tpl.php}
 	</body>
 </html>
