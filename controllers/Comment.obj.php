1911b65940e94bc2cc681a35a4a8d73ef9ca5fe2
Jurgens du Toit <jrgns@jrgns.net>
2009-10-18 16:01:56 +0200

    Only show comments when there is content

--- controllers/Comment.obj.php
+++ controllers/Comment.obj.php
@@ -35,7 +35,7 @@ class Comment extends TableCtl {
 	}
 	
 	public static function hook_post_display($object) {
-		if (Controller::parameter('area') == 'content' && in_array(Controller::parameter('action'), array('display'))) {
+		if ($object instanceof DBObject && Controller::parameter('area') == 'content' && in_array(Controller::parameter('action'), array('display'))) {
 			$comments = self::getComments($object->array['id']);
 			//Don't add Content, only render it.
 			Backend::add('obj_comments', $comments);
