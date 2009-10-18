34151648d06b39b83c30be44b99bc8c8efa12151
Jurgens du Toit <jrgns@jrgns.net>
2009-10-19 00:30:16 +0200

    No accept headers

--- classes/Controller.obj.php
+++ classes/Controller.obj.php
@@ -262,6 +262,8 @@ class Controller {
 				$view_name = 'HtmlView';
 			} else if (count($mime_ranges) == 1 && $mime_ranges[0]['main_type'] == '*' && $mime_ranges[0]['sub_type'] == '*') {
 				$view_name = Backend::getConfig('backend.default.view', 'HtmlView');
+			} else if (empty($mime_ranges)) {
+				$view_name = Backend::getConfig('backend.default.view', 'HtmlView');
 			}
 		}
 		if (!Component::isActive($view_name)) {
