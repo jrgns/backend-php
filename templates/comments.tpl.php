7f83115b86cd1bfefda642494fe0f26670bf5a8d
Jurgens du Toit <jrgns@jrgns.net>
2009-10-31 00:42:18 +0200

    Comment tweaking

--- templates/comments.tpl.php
+++ templates/comments.tpl.php
@@ -1,15 +1,15 @@
 <div id="comments_container">
 	<h3>Comments</h3>
-	<div id="name_container">
-		<label id="_label" for="">Name:</label><input type="text" name="" id="" class="text"/>
+	<div id="title_container">
+		<label id="comment_title_label">Title:</label><input type="text" name="comment_title" id="comment_title" class="title">
 	</div>
-	<div id="comment_container">
-		<input type="text" name="comment_title" id="comment_title" class="title"></textarea>
-	</div>
-	<div id="comment_container">
+	<div id="content_container">
 		<textarea name="comment_content" id="comment_content" class="comment"></textarea>
 	</div>
-	<?php if ($obj_comments): ?>
+	<?php if ($comment_list): var_dump($comment_list); die; ?>
+		<?php foreach($comment_list as $comment): ?>
+			{tpl:comment.tpl.php}
+		<?php endforeach; ?>
 	<?php else: ?>
 		No comments yet... Be the first to add one!
 	<?php endif; ?>
