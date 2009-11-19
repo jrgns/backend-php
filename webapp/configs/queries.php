cea8cdab7259423c69f201f6c3c3e338e776cbf7
Jurgens du Toit <jrgns@jrgns.net>
2009-11-19 08:32:50 +0200

    Prelim queries definition

---
+++ webapp/configs/queries.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Queries aliases can be defined in this file
+ *
+ * For example: $queries[product/:num] = 'product/display/$1/';
+ * maps mysite.co.za/product/3 to mysite.co.za/product/display/3
+ */
+$queries = array();
+//Edit from here:
+
+
+//Stop editing here.
+//Some system defined queries:
+$queries[':table_ctl/:num'] = '$1/display/$2';
