0221e55073e83407b36b28c512d7fad5882e3461
Jurgens du Toit <jrgns@jrgns.net>
2009-12-28 08:23:21 +0200

    Install permissions for home

--- controllers/Home.obj.php
+++ controllers/Home.obj.php
@@ -2,7 +2,13 @@
 class Home extends AreaCtl {
 	function html_index() {
 		Backend::add('Sub Title', 'Welcome');
-		Controller::addContent('Welcome to the Backend');
+		Controller::addContent('Welcome to #Title#');
 		return true;
+	}
+	
+	public static function install(array $options = array()) {
+		$toret = parent::install($options);
+		
+		$toret = Permission::add('anonymous', 'index', 'home') && $toret;
 	}
 }
