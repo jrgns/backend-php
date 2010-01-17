24b436e989117b987a5ecd2aba26f92c715751dd
Jurgens du Toit <jrgns@jrgns.net>
2010-01-17 14:39:42 +0200

    Use time_elapsed in comment.tpl.php

--- templates/comment.tpl.php
+++ templates/comment.tpl.php
@@ -1,7 +1,7 @@
 	<div>
 		<img src="http://www.gravatar.com/avatar.php?size=30&d=identicon&gravatar_id=<?php echo md5(strtolower($comment['email'])) ?>" style="float: right;">
 		<p class="large loud">
-			<?php echo $comment['username'] ?> at <?php echo $comment['added'] ?>
+			<?php echo $comment['username'] ?> <span title="<?php echo $comment['added'] ?>"><?php echo time_elapsed($comment['added']) ?></a>
 		</p>
 		<p>
 			<?php echo $comment['content'] ?>
