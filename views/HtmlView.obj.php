a4c3e711ef8e31bd871d9c3a4fed43ce8dfd6487
Jurgens du Toit <jrgns@jrgns.net>
2010-01-22 09:03:56 +0200

    Only display default template if no display function is defined

--- views/HtmlView.obj.php
+++ views/HtmlView.obj.php
@@ -23,7 +23,8 @@ class HtmlView extends View {
 	}
 	
 	public static function hook_display($results, $controller) {
-		if (Permission::check(Controller::$action, Controller::$area)) {
+		$display_method = Controller::$view->mode . '_' . Controller::$action;
+		if (Permission::check(Controller::$action, Controller::$area) && !method_exists($controller, $display_method)) {
 			$template_file = Controller::$area . '.' . Controller::$action . '.tpl.php';
 			if (Render::checkTemplateFile($template_file)) {
 				Controller::addContent(Render::renderFile($template_file, $results));
