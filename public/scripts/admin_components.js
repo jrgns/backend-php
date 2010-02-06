564dd4e0bd2e4929b273057036a6c177088aedd2
Jurgens du Toit <jrgns@jrgns.net>
2010-02-06 17:04:23 +0200

    Show loading gif when updating component

--- public/scripts/admin_components.js
+++ public/scripts/admin_components.js
@@ -5,13 +5,18 @@ $(document).ready(function() {
 function toggleActive(evt) {
 	var id = $(this).attr('id').replace(/^component_/, '');
 	if (id) {
-		$.getJSON('?q=component/toggle/' + id + '/active&mode=json', function(json) {
-			if (json['active'] == 1) {
-				$('a#component_' + json['id']).html('Yes');
-			} else {
-				$('a#component_' + json['id']).html('No');
-			}
-		});
+		$('a#component_' + id).html('<img src="images/loading1.gif" alt="Loading..."/>');
+		try {
+			$.getJSON('?q=component/toggle/' + id + '/active&mode=json', function(json) {
+				if (json['active'] == 1) {
+					$('a#component_' + json['id']).html('Yes');
+				} else {
+					$('a#component_' + json['id']).html('No');
+				}
+			});
+		} catch (e) {
+			$('a#component_' + id).html('JS Error');
+		}
 	}
 	return false;
 }
