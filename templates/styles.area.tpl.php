4790652213c1a20bebb8c22f232a75071974fad6
Jurgens du Toit <jurgensd@brandedinternet.co.za>
2010-02-25 14:52:46 +0200

    Split styles.tpl into application and area specific

---
+++ templates/styles.area.tpl.php
@@ -0,0 +1,8 @@
+	<?php if (Component::isActive('Style')): ?>
+		<link rel="stylesheet" href="<?php echo SITE_LINK ?>style/#Area#/#Action#.css" type="text/css">
+	<?php endif; ?>
+<?php if (!empty($Styles)): ?>
+	<?php foreach($Styles as $style): ?>
+		<link type="text/css" rel="stylesheet" href="<?php echo $style ?>">
+	<?php endforeach; ?>
+<?php endif; ?>
--- templates/styles.tpl.php
+++ templates/styles.tpl.php
@@ -1,15 +1,2 @@
-		<link rel="stylesheet" href="<?php echo SITE_LINK ?>styles/blueprint/screen.css" type="text/css" media="screen, projection">
-		<link rel="stylesheet" href="<?php echo SITE_LINK ?>styles/blueprint/print.css" type="text/css" media="print"> 
-		<link rel="stylesheet" href="<?php echo SITE_LINK ?>styles/blueprint/tabs.css" type="text/css" media="screen, projection"> 
-		<!--[if IE]>
-		<link rel="stylesheet" href="styles/blueprint/ie.css" type="text/css" media="screen, projection">
-		<![endif]-->
-		<link rel="stylesheet" href="<?php echo SITE_LINK ?>styles/basic.css" type="text/css">
-		<?php if (Component::isActive('Style')): ?>
-			<link rel="stylesheet" href="<?php echo SITE_LINK ?>style/#Area#/#Action#.css" type="text/css">
-		<?php endif; ?>
-<?php if (!empty($Styles)): ?>
-	<?php foreach($Styles as $style): ?>
-		<link type="text/css" rel="stylesheet" href="<?php echo $style ?>">
-	<?php endforeach; ?>
-<?php endif; ?>
+		{tpl:styles.application.tpl.php}
+		{tpl:styles.area.tpl.php}
