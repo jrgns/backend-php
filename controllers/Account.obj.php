f9b7eb4d991312f926b08932f2728b79aaf91cab
Jurgens du Toit <jrgns@jrgns.net>
2010-03-09 23:58:03 +0200

    Debugged

--- controllers/Account.obj.php
+++ controllers/Account.obj.php
@@ -1,6 +1,7 @@
 <?php
 class Account extends BackendAccount {
 	public static function install(array $options = array()) {
-		Value::set('BackendAccount', get_class($this));
+		Value::set('BackendAccount', 'Account');
+		return true;
 	}
 }
