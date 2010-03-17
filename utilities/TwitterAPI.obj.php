3d5670334c04c927af5c929dc57837fae5bf87d0
Jurgens du Toit <jrgns@jrgns.net>
2010-03-17 08:21:35 +0200

    Use error_msg property instead of Backend messaging system for TwitterAPI

--- utilities/TwitterAPI.obj.php
+++ utilities/TwitterAPI.obj.php
@@ -1,6 +1,7 @@
 <?php
 class TwitterAPI {
 	public static $started      = false;
+	public static $error_msg    = false;
 	private static $auth_token  = false;
 	private static $auth_secret = false;
 	
@@ -25,45 +26,62 @@ class TwitterAPI {
 	}
 
 	public static function search($parameter) {
+		self::$error_msg = false;
 		$result = curl_request('http://search.twitter.com/search.json?q=' . $parameter);
 		if ($result = json_decode($result)) {
 			if (array_key_exists('error', $result)) {
-				Backend::addError($result->error);
+				self::$error_msg = $result->error;
 			} else {
 				return is_object($result) && isset($result->results) ? $result->results : false;
 			}
+		} else {
+			self::$error_msg = 'Invalid JSON returned: ' . $result;
 		}
 		return false;
 	}
 	
 	public static function mentions() {
+		self::$error_msg = false;
 		if (self::started()) {
 			$parameters = array('oauth_token' => self::$auth_token, 'oauth_token_secret' => self::$auth_secret);
 			$result = OAuth::request('http://twitter.com/statuses/mentions.json', $parameters);
 			if ($result = json_decode($result)) {
 				if (array_key_exists('error', $result)) {
-					Backend::addError($result->error);
+					self::$error_msg = $result->error;
+				} else if (!is_array($result)) {
+					self::$error_msg = 'Weird Result: ' . var_export($result, true);
 				} else {
 					return is_array($result) ? $result : false;
 				}
+			} else {
+				self::$error_msg = 'Invalid JSON returned: ' . $result;
 			}
+		} else {
+			self::$error_msg = 'Could not get Authorization';
 		}
 		return false;
 	}
 
 	public static function tweet($status) {
+		self::$error_msg = false;
 		if (self::started()) {
 			$parameters = array('oauth_token' => self::$auth_token, 'oauth_token_secret' => self::$auth_secret);
 			$parameters['status'] = $status;
 			$result = OAuth::request('http://twitter.com/statuses/update.json', $parameters, 'POST');
 			if ($result = json_decode($result)) {
 				if (array_key_exists('error', $result)) {
-					Backend::addError($result->error);
+					self::$error_msg = $result->error;
+				} else if (!$result) {
+					self::$error_msg = 'Weird Result: ' . var_export($result, true);
 				} else {
 					return $result;
 				}
+			} else {
+				self::$error_msg = 'Invalid JSON returned: ' . $result;
 			}
-			return false;
+		} else {
+			self::$error_msg = 'Could not get Authorization';
 		}
+		return false;
 	}
 }
