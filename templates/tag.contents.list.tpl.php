54992a954bf893a7683299583a25621fe2e6e50c
Jurgens du Toit <jrgns@jrgns.net>
2010-03-19 09:55:26 +0200

    Updated contets tag list

--- templates/tag.contents.list.tpl.php
+++ templates/tag.contents.list.tpl.php
@@ -1,10 +1,28 @@
 <?php
 if (!empty($Object->array['list']) && is_array($Object->array['list'])):
-	foreach($Object->array['list'] as $item): ?>
-		<h3><?php echo plain($item['title']) ?></h3>
+	foreach($Object->array['list'] as $item):
+		$tags = Tag::getTags('contents', $item['id']);
+		$link = '?q=content/' . $item['name'];
+	?>
+		<h3>
+			<a href="<?php echo $link ?>"><?php echo plain($item['title']) ?></a>
+		</h3>
 		<p>
-			<?php echo Content::createPreview($item['body']) ?>
+			<?php echo Content::createPreview($item['body'], false) ?>
 		</p>
+		<div class="clear">
+			<div class="span-3">
+				<a href="<?php echo $link ?>">Read more</a>
+			</div>
+			<div class="span-6">
+				<?php if ($tags): foreach($tags as $tag): ?>
+					<a  href="?q=tag/<?php echo $tag['id'] ?>"><?php echo $tag['name'] ?></a>
+				<?php endforeach; endif; ?>
+			</div>
+			<div style="text-align: right;" class="small quiet">
+				Last updated <?php echo time_elapsed($item['modified']) ?>
+			</div>
+		</div>
 		<hr>
 	<?php endforeach;
 endif;
