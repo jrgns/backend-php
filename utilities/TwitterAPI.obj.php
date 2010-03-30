cc02225ca569395dc259ac530cf04d12708cdf2f
Jurgens du Toit <jrgns@jrgns.net>
2010-03-30 06:15:12 +0200

    Refactored Twitter API

--- utilities/TwitterAPI.obj.php
+++ utilities/TwitterAPI.obj.php
@@ -27,60 +27,66 @@ class TwitterAPI {
 
 	public static function search($parameter) {
 		self::$error_msg = false;
-		$result = curl_request('http://search.twitter.com/search.json?q=' . $parameter);
-		if ($result = json_decode($result)) {
-			if (array_key_exists('error', $result)) {
-				self::$error_msg = $result->error;
-			} else {
-				return is_object($result) && isset($result->results) ? $result->results : false;
-			}
+		$returned = curl_request('http://search.twitter.com/search.json?q=' . $parameter);
+		if (!$returned) {
+			self::$error_msg = 'Invalid Twitter API request';
+			return false;
+		} else if (!($result = json_decode($returned))) {
+			self::$error_msg = 'Invalid JSON returned: ' . $returned;
+			return false;
+		}
+		if (array_key_exists('error', $result)) {
+			self::$error_msg = $result->error;
 		} else {
-			self::$error_msg = 'Invalid JSON returned: ' . $result;
+			return is_object($result) && isset($result->results) ? $result->results : false;
 		}
 		return false;
 	}
 	
 	public static function mentions() {
 		self::$error_msg = false;
-		if (self::started()) {
-			$parameters = array('oauth_token' => self::$auth_token, 'oauth_token_secret' => self::$auth_secret);
-			$result = OAuth::request('http://api.twitter.com/1/statuses/mentions.json', $parameters);
-			if ($result = json_decode($result)) {
-				if (array_key_exists('error', $result)) {
-					self::$error_msg = $result->error;
-				} else if (!is_array($result)) {
-					self::$error_msg = 'Weird Result: ' . var_export($result, true);
-				} else {
-					return is_array($result) ? $result : false;
-				}
-			} else {
-				self::$error_msg = 'Invalid JSON returned: ' . $result;
-			}
-		} else {
+		if (!self::started()) {
 			self::$error_msg = 'Could not get Authorization';
+			return false;
+		}
+		$parameters = array('oauth_token' => self::$auth_token, 'oauth_token_secret' => self::$auth_secret);
+		$returned = OAuth::request('http://api.twitter.com/1/statuses/mentions.json', $parameters);
+		if (!$returned) {
+			self::$error_msg = 'Invalid Twitter API request';
+			return false;
+		} else if (!($result = json_decode($returned))) {
+			self::$error_msg = 'Invalid JSON returned: ' . $returned;
+			return false;
+		}
+
+		if (array_key_exists('error', $result)) {
+			self::$error_msg = $result->error;
+		} else {
+			return is_array($result) ? $result : false;
 		}
 		return false;
 	}
 
 	public static function tweet($status) {
 		self::$error_msg = false;
-		if (self::started()) {
-			$parameters = array('oauth_token' => self::$auth_token, 'oauth_token_secret' => self::$auth_secret);
-			$parameters['status'] = $status;
-			$result = OAuth::request('http://api.twitter.com/1/statuses/update.json', $parameters, 'POST');
-			if ($result = json_decode($result)) {
-				if (array_key_exists('error', $result)) {
-					self::$error_msg = $result->error;
-				} else if (!$result) {
-					self::$error_msg = 'Weird Result: ' . var_export($result, true);
-				} else {
-					return $result;
-				}
-			} else {
-				self::$error_msg = 'Invalid JSON returned: ' . $result;
-			}
-		} else {
+		if (!self::started()) {
 			self::$error_msg = 'Could not get Authorization';
+			return false;
+		}
+		$parameters = array('oauth_token' => self::$auth_token, 'oauth_token_secret' => self::$auth_secret);
+		$parameters['status'] = $status;
+		$returned = OAuth::request('http://api.twitter.com/1/statuses/update.json', $parameters, 'POST');
+		if (!$returned) {
+			self::$error_msg = 'Invalid Twitter API request';
+			return false;
+		} else if (!($result = json_decode($returned))) {
+			self::$error_msg = 'Invalid JSON returned: ' . $returned;
+			return false;
+		}
+		if (array_key_exists('error', $result)) {
+			self::$error_msg = $result->error;
+		} else {
+			return $result;
 		}
 		return false;
 	}
