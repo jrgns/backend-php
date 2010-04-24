644bf87f1b8e16e7d4ed42a0b508115dd233208e
Jurgens du Toit <jrgns@jrgns.net>
2010-04-24 11:42:53 +0200

    Meta description update

--- templates/head.tpl.php
+++ templates/head.tpl.php
@@ -5,11 +5,10 @@
 		<?php if ($author = Backend::getConfig('application.author')): ?>
 			<meta name="author" content="<?php echo $author ?>">
 		<?php endif; ?>
-		<?php if ($description = Backend::getConfig('application.description')): ?>
-			<meta name="description" content="<?php echo $description ?>">
+		<?php if (!empty($meta_description) || $meta_description = Backend::getConfig('application.description')): ?>
+			<meta name="description" content="<?php echo $meta_description ?>">
 		<?php endif; ?>
 		<meta name="generator" content="backend-php.net">
 		<?php if (!empty($keywords)): $keywords = is_array($keywords) ? implode(', ', $keywords) : $keywords; ?>
 			<meta name="keywords" content="<?php echo $keywords ?>">
 		<?php endif; ?>
-		
