8662a67b34343e7f327cce8c11afc67f7da151a9
Jurgens du Toit <jrgns@narnia>
2010-04-26 19:00:33 +0200

    Removed Controller:: update

--- utilities/UpdateQuery.obj.php
+++ utilities/UpdateQuery.obj.php
@@ -17,7 +17,6 @@ class UpdateQuery extends Query {
 	protected $data = array();
 
 	function __construct($table, array $options = array()) {
-		Controller::$debug = true;
 		parent::__construct('UPDATE', $table, $options);
 	}
 
