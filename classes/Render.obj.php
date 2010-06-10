a3e88b47145f877a5c82d42f388f81893e9df0c6
Jurgens du Toit <jrgns@jrgns.net>
2010-06-10 07:43:05 +0200

    Wurfl Tweak

--- classes/Render.obj.php
+++ classes/Render.obj.php
@@ -118,15 +118,9 @@ class Render {
 
 		$mobile = false;
 		if (Component::isActive('Wurfl')) {
-			try {
-				$device = Wurfl::getDevice();
-				if (($device && $device->getCapability('mobile_browser') != '') || array_key_exists('mobile', $_REQUEST)) {
-					$mobile = true;
-				}
-			} catch (Exception $e) {
-				if (Controller::$debug) {
-					Backend::addError('Wurfl Error: ' . $e->getMessage());
-				}
+			$device = Wurfl::getDevice();
+			if (($device && $device->getCapability('mobile_browser') != '') || array_key_exists('mobile', $_REQUEST)) {
+				$mobile = true;
 			}
 		}
 
