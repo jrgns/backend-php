186b13ea11f39454a036fbb668db8bae93eaba6f
Jurgens du Toit <jrgns@jrgns.net>
2010-09-09 12:05:35 +0200

    std_display Tweak

--- templates/std_display.tpl.php
+++ templates/std_display.tpl.php
@@ -2,15 +2,20 @@
 	$fields = $Object->getMeta('fields');
 	$odd = false;
 	foreach($fields as $name => $field) {
-		switch ($field) {
-			case 'title';
-				$class = 'large bottom';
-				break;
-			default:
-				$class = '';
-		}
 		if (in_array($field, array('primarykey'))) {
 			continue;
+		}
+		$text  = '<?php echo empty($Object->array[\'' . $name . '\']) ? \'&nbsp;\' : plain($Object->array[\'' . $name . '\']) ?>';
+		switch ($field) {
+		case 'title';
+			$class = 'large bottom';
+			break;
+		case 'boolean':
+			$text  = '<?php echo empty($Object->array[\'' . $name . '\']) ? \'No\' : \'Yes\' ?>';
+			break;
+		default:
+			$class = '';
+			break;
 		}
 		$odd = $odd ? false : true;
 ?>
@@ -18,7 +23,7 @@
 			<div class="span-3" style="text-align: right;">
 				<label><?php echo humanize($name) ?>:</label>
 			</div>
-			<span><?php echo '<?php echo empty($Object->array[\'' . $name . '\']) ? \'&nbsp;\' : plain($Object->array[\'' . $name . '\']) ?>' ?></span>
+			<span><?php echo $text ?></span>
 		</div>
 <?php }
 } else { ?>
