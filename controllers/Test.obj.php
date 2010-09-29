04bcc2f170d1090834f194931ce361558bc940ba
Jurgens du Toit <jurgensd@brandedinternet.co.za>
2010-09-29 09:59:11 +0200

    Allow for component test

--- controllers/Test.obj.php
+++ controllers/Test.obj.php
@@ -10,16 +10,21 @@ class Test extends AreaCtl {
 		$end_result = true;
 		$results = array();
 		foreach(Component::getActive() as $component) {
+			$results[$component['name']] = array();
+			if (method_exists($component['name'], 'test')) {
+				$results[$component['name']]['component'] = call_user_func(array($component['name'], 'test'));
+			}
+
 			$methods = get_class_methods($component['name']);
 			if (!$methods) {
 				continue;
 			}
-			$results[$component['name']] = array();
 			$component_obj = new $component['name']();
 			foreach($methods as $method) {
 				if (substr($method, 0, 7) == 'action_') {
 					$test_method = preg_replace('/^action_/', 'test_', $method);
 					if (in_array($test_method, $methods)) {
+						set_time_limit(30);
 						if ($result = $component_obj->$test_method()) {
 						} else {
 							Backend::addError($component['name'] . '::' . $method . ' Failed');
@@ -29,10 +34,8 @@ class Test extends AreaCtl {
 					}
 				}
 			}
-			if (!count($results[$component['name']])) {
-				unset($results[$component['name']]);
-			}
 		}
+		$results = array_filter($results, 'count');
 		ksort($results);
 		return $return_boolean ? $end_result : $results;
 	}
