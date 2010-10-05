cd7c7bfa59cfeaec745299d4fe904a89403063c5
Jurgens du Toit <jurgensd@brandedinternet.co.za>
2010-10-05 14:08:16 +0200

    Reformatted API docs

--- templates/api_function.tpl.php
+++ templates/api_function.tpl.php
@@ -1,44 +1,45 @@
 <div class="description"><?php echo markdown($definition['description']) ?></div>
-<dl>
-	<dt>Example</dt>
-	<dd>
-		<?php if (!empty($definition['example'])): ?>
-			<?php echo $definition['example'] ?>
-		<?php else: ?>
-			<code>
-				#SITE_LINK#?q=<?php echo class_for_url($class) . '/' . $function ?><?php if (!empty($definition['parameters'])): ?><?php echo '/$' . implode('/$', array_keys($definition['parameters'])) ?><?php endif; ?>
-			</code>(Generated)
-		<?php endif; ?>
-	</dd>
-	<dt>Return: <span class="large bottom"><code><strong><?php echo $definition['return']['type'] ?></strong></code></span></dt>
-	<dd>
-		<?php echo $definition['return']['description'] ?>
-	</dd>
-	<dt>URL Parameters</dt>
-	<dd>
-		<?php if (!empty($definition['parameters'])): foreach($definition['parameters'] as $name => $parameter): ?>
-			{tpl:api_parameter.tpl.php}
-		<?php endforeach; else: ?>
-			None
-		<?php endif; ?>
-	</dd>
-	<dt>Required Parameters</dt>
-	<dd>
-		<?php if (!empty($definition['required'])): foreach($definition['required'] as $name => $parameter): ?>
-			{tpl:api_parameter.tpl.php}
-		<?php endforeach; else: ?>
-			None
-		<?php endif; ?>
-	</dd>
-	<dt>Optional Parameters</dt>
-	<dd>
-		<?php if (!empty($definition['optional'])): foreach($definition['optional'] as $name => $parameter): ?>
-			{tpl:api_parameter.tpl.php}
-		<?php endforeach; else: ?>
-			None
-		<?php endif; ?>
-	</dd>
-</dl>
+<hr>
+<h3>Example</h3>
+<p>
+	<?php if (!empty($definition['example'])): ?>
+		<?php echo $definition['example'] ?>
+	<?php else: ?>
+		<code>
+			#SITE_LINK#?q=<?php echo class_for_url($class) . '/' . $function ?><?php if (!empty($definition['parameters'])): ?><?php echo '/$' . implode('/$', array_keys($definition['parameters'])) ?><?php endif; ?>
+		</code>(Generated)
+	<?php endif; ?>
+</p>
+<hr>
+<h3>Return</h3>
+<p>
+	<span class="large">
+		<strong><?php echo $definition['return']['type'] ?></strong>
+	</span>
+	<br>
+	<?php echo $definition['return']['description'] ?>
+</p>
+<hr>
+<h3>URL Parameters</h3>
+<?php if (!empty($definition['parameters'])): foreach($definition['parameters'] as $name => $parameter): ?>
+	{tpl:api_parameter.tpl.php}
+<?php endforeach; else: ?>
+	<p>None</p>
+<?php endif; ?>
+<hr>
+<h3>Required Parameters</h3>
+<?php if (!empty($definition['required'])): foreach($definition['required'] as $name => $parameter): ?>
+	{tpl:api_parameter.tpl.php}
+<?php endforeach; else: ?>
+	<p>None</p>
+<?php endif; ?>
+<hr>
+<h3>Optional Parameters</h3>
+<?php if (!empty($definition['optional'])): foreach($definition['optional'] as $name => $parameter): ?>
+	{tpl:api_parameter.tpl.php}
+<?php endforeach; else: ?>
+	<p>None</p>
+<?php endif; ?>
 <p>
 	<a href="?q=<?php echo Controller::$area . '/define/' . Controller::$parameters[0] ?>">Up</a> | <a href="?q=<?php echo Controller::$area ?>">Top</a>
 </p>
--- templates/api_parameter.tpl.php
+++ templates/api_parameter.tpl.php
@@ -1,10 +1,11 @@
-		<p class="large bottom">
-			<strong><?php echo $name ?></strong> (<code><?php echo $parameter['type'] ?></code>)
-			<?php if (!empty($parameter['optional'])): ?> - Optional<?php endif; ?><br>
-		</p>
-		<div><?php echo markdown($parameter['description']) ?>
-			<?php if(!empty($parameter['range'])): ?>
-				<br><strong>Range:</strong>
-				<ul><li><?php echo implode('</li><li>', $parameter['range']) ?></li></ul>
-			<?php endif; ?>
-		</div>
+<div>
+	<p class="large bottom">
+		<strong><?php echo $name ?></strong> (<?php echo $parameter['type'] ?>)
+		<?php if (!empty($parameter['optional'])): ?> - Optional<?php endif; ?><br>
+	</p>
+	<?php echo markdown($parameter['description']) ?>
+	<?php if(!empty($parameter['range'])): ?>
+		<br><strong>Range:</strong>
+		<ul><li><?php echo implode('</li><li>', $parameter['range']) ?></li></ul>
+	<?php endif; ?>
+</div>
