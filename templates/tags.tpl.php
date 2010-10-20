8d9a340370ae387558d2dbd3b095e84530b15c18
Jurgens du Toit <jrgns@jrgns.net>
2010-10-20 22:35:39 +0200

    Debugged

--- templates/tags.tpl.php
+++ templates/tags.tpl.php
@@ -1,5 +1,5 @@
 <div class="tags_container">
 	<?php foreach($obj_tags as $tag): ?>
-		<label><a href="?q=tag/<?php echo $tag['id'] ?>"><?php echo $tag['name'] ?></a></label>
+		<label><a href="?q=tag/<?php echo $tag['tag_id'] ?>"><?php echo $tag['name'] ?></a></label>
 	<?php endforeach; ?>
 </div>
