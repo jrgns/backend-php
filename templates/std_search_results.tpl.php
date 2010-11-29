361830ef2c8560126e3f51622a9a861bef4b0637
Jurgens du Toit <jurgensd@brandedinternet.co.za>
2010-11-29 14:22:31 +0200

    Pass down list_start and list_length

--- templates/std_search_results.tpl.php
+++ templates/std_search_results.tpl.php
@@ -6,7 +6,7 @@
 <?php if (!empty($term)): ?>
 	<p class="large">Showing results for the search for <strong>#term#</strong></p>
 	<?php if (Render::checkTemplateFile($Object->getArea() . '.list.tpl.php')): ?>
-		<?php echo Render::renderFile($Object->getArea() . '.list.tpl.php'); ?>
+		<?php echo Render::renderFile($Object->getArea() . '.list.tpl.php', array('list_start' => $list_start, 'list_length' => $list_length)); ?>
 	<?php else: ?>
 		{tpl:std_list.tpl.php}
 	<?php endif; ?>
