aa37f13d4da67391af23bd9f380074615af4e031
Jurgens du Toit <jurgensd@brandedinternet.co.za>
2011-01-17 08:24:51 +0200

    return_bytes function

--- functions.inc.php
+++ functions.inc.php
@@ -641,3 +641,24 @@ function stripslashes_deep($value) {
 
 	return $value;
 }
+
+/**
+ * Convert PHP shorthand notation to bytes
+ *
+ * From http://www.php.net/manual/en/function.ini-get.php on 2011-01-17
+ */
+function return_bytes($val) {
+    $val = trim($val);
+    $last = strtolower($val[strlen($val)-1]);
+    switch($last) {
+        // The 'G' modifier is available since PHP 5.1.0
+        case 'g':
+            $val *= 1024;
+        case 'm':
+            $val *= 1024;
+        case 'k':
+            $val *= 1024;
+    }
+
+    return $val;
+}
