b850e2deaecaa26067d1c6e5e2c64bfd6bcd9547
Jurgens du Toit <jurgensd@brandedinternet.co.za>
2011-01-18 22:31:16 +0200

    Prelim BackendLock Module

---
+++ models/BackendSystemLockObj.obj.php
@@ -0,0 +1,20 @@
+<?php
+class BackendSystemLockObj extends BackendLockObj {
+	public function __construct(BackendLockObj $object) {
+		foreach($object as $name => $value) {
+			$this->$name = $value;
+		}
+	}
+	
+	function validate($data, $action, $options = array()) {
+		$toret = true;
+		$data = parent::validate($data, $action, $options);
+		if ($data) {
+			if (!empty($data['expire'])) {
+				Backend::addError('System Locks must expire');
+				$toret = false;
+			}
+		}
+		return $toret ? $data : false;
+	}
+}
