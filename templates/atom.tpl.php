6fb8ad66802385e5bdab232bba7fecd85dd13448
Jurgens du Toit <jrgns@jrgns.net>
2011-04-08 09:43:42 +0200

    Debugged atom feed

--- templates/atom.tpl.php
+++ templates/atom.tpl.php
@@ -2,15 +2,16 @@
 <feed xmlns="http://www.w3.org/2005/Atom">
 	<title><?php echo htmlspecialchars($title) ?></title>
 	<?php if (!empty($subtitle)): ?><subtitle><?php echo htmlspecialchars($sub_title) ?></subtitle><?php endif; ?>
-	<link><?php echo $link ?></link>
+	<link href="<?php echo $link ?>" rel="self" />
+	<id><?php echo $link ?></id>
 	<description><?php echo htmlspecialchars($description) ?></description>
 	<updated><?php echo date('r') ?></updated>
 	<?php if ($list) foreach($list as $item): ?>
 		<entry>
 			<title><?php echo htmlspecialchars($item['title']) ?></title>
-			<link><?php echo $item['link'] ?></link>
-			<summary type="html"><![CDATA[<?php echo $item['body'] ?>]]></summary>
-			<updated><?php echo date('r', strtotime($item['added'])) ?></updated>
+			<link href="<?php echo $item['link'] ?>" />
+			<summary type="html"><![CDATA[<?php echo Content::createPreview($item['body']) ?>]]></summary>
+			<updated><?php echo gmdate('r', strtotime($item['modified'])) ?></updated>
 			<id><?php echo $item['link'] ?></id>
 		</entry>
 	<?php endforeach; ?>
