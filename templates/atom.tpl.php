7ab1e5ceb58191daaba85a1b507019a3174b32cb
Jurgens du Toit <jrgns@jrgns.net>
2011-04-08 09:54:03 +0200

    Added author info to atom feed

--- templates/atom.tpl.php
+++ templates/atom.tpl.php
@@ -4,8 +4,23 @@
 	<?php if (!empty($subtitle)): ?><subtitle><?php echo htmlspecialchars($sub_title) ?></subtitle><?php endif; ?>
 	<link href="<?php echo $link ?>" rel="self" />
 	<id><?php echo $link ?></id>
-	<description><?php echo htmlspecialchars($description) ?></description>
 	<updated><?php echo date('r') ?></updated>
+	<?php $author = Backend::getConfig('application.author'); ?>
+	<?php if (is_string($author)): ?>
+		<author><name><?php echo $author ?></name></author>
+	<?php elseif (is_array($author)): ?>
+		<author>
+			<?php if (array_key_exists('name', $author)): ?>
+				<name><?php echo $author['name'] ?></name>
+			<?php endif; ?>
+			<?php if (array_key_exists('email', $author)): ?>
+				<email><?php echo $author['email'] ?></email>
+			<?php endif; ?>
+			<?php if (array_key_exists('website', $author)): ?>
+				<uri><?php echo $author['website'] ?></uri>
+			<?php endif; ?>
+		</author>
+	<?php endif; ?>
 	<?php if ($list) foreach($list as $item): ?>
 		<entry>
 			<title><?php echo htmlspecialchars($item['title']) ?></title>
