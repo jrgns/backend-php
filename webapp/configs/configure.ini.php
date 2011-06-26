0115a11521c7a58ffef3f3f5b7ccdac345651177
Jurgens du Toit <jrgns@jrgns.net>
2011-06-26 04:59:46 +0200

    New config file

---
+++ webapp/configs/configure.ini.php
@@ -0,0 +1,18 @@
+;<?php die(); ?>
+[settings]
+Class = "Application"
+DefaultView = "HtmlView"
+UseCache = "1"
+TemplateLocation = "templates"
+ConfigValueSet = "1"
+[application]
+Title = "Backend"
+Moto = 
+HelpBoxContent = 
+Description = 
+LogToFile = 
+AdminInstalled = "2011-06-26 04:56:07"
+[author]
+Name = 
+Email = 
+Website = 
