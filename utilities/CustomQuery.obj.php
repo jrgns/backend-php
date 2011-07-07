3874635283489fcfebe4919d6cb5e102841573aa
Shaun Morrow <shaun@brandedinternet.co.za>
2011-07-07 14:21:31 +0200

    Adding getCount function for read on customquery

--- utilities/CustomQuery.obj.php
+++ utilities/CustomQuery.obj.php
@@ -3,4 +3,14 @@ class CustomQuery extends Query {
 	function __construct($query, array $options = array()) {
 		$this->setQuery($query, $options);
 	}
+	
+	function getCount($parameters) {
+		$this->buildTable();
+		$count_query = clone($this);
+		$count_query
+			->setOrder(array())
+			->setGroup(array());
+		$count_query = new CustomQuery(preg_replace(REGEX_MAKE_COUNT_QUERY, '$1 COUNT(*) $3', $count_query));
+		return $count_query->fetchColumn($parameters);
+	}
 }
