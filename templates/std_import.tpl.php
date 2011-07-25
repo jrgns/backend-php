6e121f3a899b328571da813568f4959328c2ca11
Jurgens du Toit <jurgensd@brandedinternet.co.za>
2011-07-25 20:32:30 +0200

    Tweaked import template

--- templates/std_import.tpl.php
+++ templates/std_import.tpl.php
@@ -1,10 +1,10 @@
 <?php if (!empty($Object)): ?>
 	<form method="post" action="?q=<?php echo array_key_exists('q', $_REQUEST) ? $_REQUEST['q'] : '' ?>" enctype="multipart/form-data">
-	<?php 
+	<?php
 		$fields = $Object->getMeta('fields');
 		?>
-		<input type="file" name="import_file" class="text" />
-		<input type="submit" value="Import <?php echo get_class($Object->getMeta('name')) ?>" class=""/>
+		<input type="file" name="import_file" class="text" /><br>
+		<input type="submit" value="Import <?php echo $Object->getMeta('name') ?>" class=""/>
 	</form>
 <?php else: Backend::addNotice('No Object to Import'); ?>
 	No object
