896434958c9f6431a00a761b605ce3239257d0b7
Jurgens du Toit <jurgensd@brandedinternet.co.za>
2011-08-15 17:18:32 +0200

    Fixed SITE_LINK without slash

--- templates/styles.application.tpl.php
+++ templates/styles.application.tpl.php
@@ -2,6 +2,6 @@
 		<link rel="stylesheet" href="<?php echo SITE_LINK ?>/styles/blueprint/print.css" type="text/css" media="print">
 		<link rel="stylesheet" href="<?php echo SITE_LINK ?>/styles/blueprint/tabs.css" type="text/css" media="screen, projection">
 		<!--[if IE]>
-		<link rel="stylesheet" href="<?php echo SITE_LINK ?>styles/blueprint/ie.css" type="text/css" media="screen, projection">
+		<link rel="stylesheet" href="<?php echo SITE_LINK ?>/styles/blueprint/ie.css" type="text/css" media="screen, projection">
 		<![endif]-->
 		<link rel="stylesheet" href="<?php echo SITE_LINK ?>/styles/basic.css" type="text/css">
