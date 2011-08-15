95f3561ad41508196309670166531cd8f878266e
Jurgens du Toit <jurgensd@brandedinternet.co.za>
2011-08-15 17:04:47 +0200

    Template Tweaks

--- templates/backend_errors.tpl.php
+++ templates/backend_errors.tpl.php
@@ -1,4 +1,4 @@
-<div class="error span-23 prepend-1" id="backend_error_container"<?php if (!isset($BackendErrors) || count($BackendErrors) == 0): ?> style="display: none;"<?php endif; ?>>
+<div class="error span-23 last" id="backend_error_container"<?php if (!isset($BackendErrors) || count($BackendErrors) == 0): ?> style="display: none;"<?php endif; ?>>
 	<ul class="bottom loud large" id="backend_error">
 		<?php if (isset($BackendErrors) && count($BackendErrors)): ?>
 			<?php foreach($BackendErrors as $error): ?>
@@ -7,4 +7,3 @@
 		<?php endif; ?>
 	</ul>
 </div>
-
