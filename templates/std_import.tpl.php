52c06de6c0e56f0556f31bd3af3f9219a9f27c0c
Jurgens du Toit <jrgns@jrgns.net>
2011-09-12 18:15:59 +0200

    Cleanup

--- templates/std_import.tpl.php
+++ templates/std_import.tpl.php
@@ -5,9 +5,6 @@ $action_url = empty($action_url) ?
 $action_name = empty($action_name) ? ucwords(Controller::$action) : $action_name;
 if (!empty($db_object)): ?>
 	<form method="post" action="?q=<?php echo $action_url ?>" enctype="multipart/form-data">
-	<?php
-		$fields = $db_object->getMeta('fields');
-		?>
 		<input type="file" name="import_file" class="text" />
 		<input type="submit" value="<?php echo $action_name ?> <?php echo $db_object->getMeta('name') ?>" class=""/>
 	</form>
